3f7e3a34ccd477e3692a7fcc33ca6b9a9537a2e7
Chokri NOUAR <chokri.nouar@gmail.com>
2026-01-02 13:28:21 +0100

    main.c

---
+++ version en C/main.c
@@ -0,0 +1,17 @@
+// main.c
+#include <stdio.h>
+#include "nmgm_prbg.h"
+
+int main() {
+    const char* password = "ABIDINE";
+    long F = 1000000; // 1 million de bits
+
+    if (generate_bitstream(password, F) == 0) {
+        printf("✅ %ld bits générés et enregistrés dans SuitesNMGM.txt\n", F);
+    } else {
+        fprintf(stderr, "❌ Échec de la génération.\n");
+        return 1;
+    }
+
+    return 0;
+}
